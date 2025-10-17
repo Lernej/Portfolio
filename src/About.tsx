bb0a9dd4abbba7d1e2e5509469334089a731dd99
Lernej <noahklern@gmail.com>
2025-10-17 16:11:55 -0400

    Mountain build, pre space update

---
+++ src/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="mt-[3rem] h-[calc(100vh-3rem)] bg-white text-black flex items-center justify-center">
+      hola
+    </div>
+  );
+};
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,26 @@
 import "./App.css";
-import Headshot from "./components/Headshot";
-import Title from "./components/Title";
-import Clouds from "./components/Clouds";
-import Waves from "./components/Waves";
-
-import Snowfall from "react-snowfall";
-import ResponsiveDiv from "./components/ResponsiveDiv";
-import Button from "./components/Button";
-import Mt from "./assets/mountain.svg?react";
-import PopInDiv from "./components/PopInDiv";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
 
 function App() {
   return (
-    <>
-      <div className="z-100 flex bg-gray-400 h-12 w-full fixed justify-start items-center gap-10 pl-5 rounded-b-sm shadow-lg text-white font-medium top-0 right-0 left-0">
-        <button className="rounded-3x1 p-1 cursor-pointer">Home</button>
-      </div>
-
-      <div className="relative flex min-h-screen items-center justify-center">
-        <Clouds className="absolute top-[5%] left-[2%] md:h-30"></Clouds>
-        <Clouds className="absolute bottom-[5%] right-[2%] md:h-30"></Clouds>
-
-        <div className="flex flex-col items-center justify-center gap-10 md:flex-row">
-          <Title></Title>
-
-          <Headshot></Headshot>
+    <BrowserRouter>
+      <div>
+        <div className="z-100 flex bg-gray-400 h-[3rem] w-full fixed justify-start items-center gap-10 pl-5 rounded-b-sm shadow-lg text-white font-medium top-0 right-0 left-0">
+          <a href="/">
+            {" "}
+            <button className="rounded-3x1 p-1 cursor-pointer">Home</button>
+          </a>
         </div>
-
-        <div className="flex flex-col items-center text-white absolute bottom-5 motion-preset-blink motion-duration-5000 ">
-          <div>Scroll to view more</div>
-          <div>↓</div>
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
         </div>
       </div>
-
-      <div className="min-h-screen bg-gray-600 relative">
-        <div className="absolute inset-0 z-50">
-          <Waves></Waves>
-        </div>
-
-        <PopInDiv className="h-0 absolute flex w-full items-center justify-center text-center z-50 top-[40%]">
-          <div className="text-white font-bold text-3xl motion-preset-pulse-sm motion-duration-5000">
-            Select a button to navigate
-          </div>
-        </PopInDiv>
-
-        <div className="absolute bottom-0 left-[3%] lg:left-[20%]">
-          <div className="relative w-[90vw] lg:w-[60vw]">
-            <PopInDiv className="">
-              <Mt className="w-[90vw] lg:w-[60vw]"></Mt>
-            </PopInDiv>
-
-            <Button className="bottom-[20%] left-[5%]">About Me</Button>
-            <Button className="bottom-[50%] left-[15%]">Projects</Button>
-            <Button className="top-0 left-[50%] lg:left-[54%]">
-              Experience
-            </Button>
-            <Button className="top-[35%] left-[75%] lg:left-[80%]">
-              Skills
-            </Button>
-          </div>
-        </div>
-
-        <div className="z-0">
-          <ResponsiveDiv />
-        </div>
-      </div>
-    </>
+    </BrowserRouter>
   );
 }
 
